a4aedbda5775f6de8fa9cf4d7df9b40ea03ebeea
sajay2497 <60282009+sajay2497@users.noreply.github.com>
2020-03-02 18:26:59 +0530

    Update product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, public router: Router) { }
   confirmationString:string = "New Product Has Been Added";
   isAdded: boolean = false;
   productObj:object = {};
@@ -20,6 +21,7 @@ export class ProductComponent implements OnInit {
     }
     this.http.post("http://localhost:3000/products", this.productObj).subscribe((res:Response)=>{
       // console.log(res);
+      this.router.navigate(['/']);
       this.isAdded = true;
       
     })
